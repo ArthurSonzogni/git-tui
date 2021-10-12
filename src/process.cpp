f9438b3ee4a0f2b0331c9330fb3e35595947c22e
ArthurSonzogni <sonzogniarthur@gmail.com>
2021-10-12 22:02:04 +0200

    Add Windows and MacOS artifacts.

--- src/process.cpp
+++ src/process.cpp
@@ -5,15 +5,24 @@
 #include <stdexcept>
 #include <string>
 
+#ifndef WIN32
+inline FILE* _popen(const char* command, const char* type) {
+  return popen(command, type);
+}
+inline void _pclose(FILE* file) {
+  pclose(file);
+}
+#endif
+
 std::string ExecuteProcess(std::string cmd) {
   std::array<char, 128> buffer;
   std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                pclose);
+  std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd.c_str(), "r"),
+                                                 _pclose);
   if (!pipe) {
     throw std::runtime_error("popen() failed!");
   }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+  while (fgets(buffer.data(), (int)buffer.size(), pipe.get()) != nullptr) {
     result += buffer.data();
   }
   return result;
