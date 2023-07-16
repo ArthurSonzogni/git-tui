0d0f56e154d3ba136c7778394f8777ce5e18788a
lucamacavero <65252677+lu-maca@users.noreply.github.com>
2023-07-16 16:20:12 +0200

    add generic git commands to git-tui command

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,21 @@
 #include "help.hpp"     // for main
 #include "log.hpp"      // for main
 #include "version.hpp"  // for main
+#include <cstdlib>      // for main
+#include <iostream>     // for main
+
+// redirect all non implemented git commands to git
+void go_through_git(int argc, const char** argv, std::string command){
+  std::string full_git_cmd = "git ";
+  full_git_cmd.append(command);
+  
+  for (int i = 0; i <  argc; i++){
+    full_git_cmd.append(" ");
+    full_git_cmd.append(argv[i]);
+  }
+  std::system(full_git_cmd.c_str());
+}
+
 
 int main(int argc, const char** argv) {
   if (argc == 0)
@@ -22,6 +37,8 @@ int main(int argc, const char** argv) {
   argc--;
   argv++;
 
+  // int arg_num = argc;
+
   if (command == "diff")
     return gittui::diff::main(argc, argv);
 
@@ -31,7 +48,8 @@ int main(int argc, const char** argv) {
   if (command == "version")
     return gittui::version::main(argc, argv);
 
-  return gittui::help::main(argc, argv);
+  go_through_git(argc, argv, command);
+  
 }
 
 // Copyright 2021 Arthur Sonzogni. All rights reserved.
